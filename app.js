be1652480e281fd5876f483b4514d7d4d9c305cb
kestoc <kevin512ocampo@gmail.com>
2021-11-28 13:24:21 -0500

    Retoques esteticos en las paginas ya hechas y enlaces de redes sociales

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+// Componente de encabezado Vue
+Vue.component('encabezado',{
+    template:
+    `
+    <div class="container-fluid mb-5 bg-primary d-flex justify-content-center">
+        <h1 class="mb-0 p-4">{{titulo}}</h1>
+    </div>
+    `,
+    data(){
+        return {
+            titulo: 'P&J'    
+        }
+    }
+})
+
 // Objeto Vue
 const page = new Vue({
-    el: '#page',
-    data: {
-        titulo: 'P&J'
-    }
+    el: '#page'
 });
